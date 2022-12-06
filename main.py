31d02711d9d963b0d2a511e2c40442bc3e992619
Turner009 <009turner@gmail.com>
2022-12-06 10:34:23 -0600

    2 user dictionaries created

---
+++ main.py
@@ -0,0 +1,11 @@
+user_one = {
+    'full_name':'',
+    'username': '',
+    'password': '',
+    'password': '',
+    'account_balance': '',
+    'connected_banks': [('bank one', 0),('bank two', 10)]
+}
+user_two = {}
+
+print(user_one['connected_banks'][1][1])
