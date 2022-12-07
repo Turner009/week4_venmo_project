1f58841be18f06a6e33d7fb14eeb108fec005d1a
Turner009 <009turner@gmail.com>
2022-12-07 14:43:06 -0600

    simplified login to work for either user

--- main.py
+++ main.py
@@ -3,45 +3,53 @@ user_one = {
     'username': 'HareJordan',
     'password': 'bugs1',
     'account_balance': 250.00,
-    'connected_banks': [('bank one', 0),('bank two', 0)]
+    'connected_banks': [('MCU', 1500.00),('Acme Savings Club', 2000.00)]
 }
 user_two = {
     'full_name':'Elmer Fudd',
     'username': 'Besthunter',
     'password': 'elmer1',
     'account_balance': 50.00,
-    'connected_banks': [('bank one', 0),('bank two', 0)]
+    'connected_banks': [('Chase', 1000.00),('Acme Savings Club', 2500.00)]
 }
 
 login = ''
 password = ''
+logged_in_user =''
+other_user = ''
 
+login = input('Enter your username please.')
 while login != user_one['username'] or user_two['username']:
-    login = input('Enter your username please.')
-    if login == user_one['username']:
-        print(f"Welcome {user_one['full_name']}")
-        password = input('Please enter your password. ')
-        while password != user_one['password']:
-            print("Incorrect password")
-            password = input('Please enter your password. ')
-            if password == user_one['password']:
-                print("")
-                print(f"Successful login your current balance is {user_one['account_balance']}.")   
-        break
-    elif login == user_two['username']:
-        print("")
-        print(f"Welcome {user_two['full_name']}")
-        print("")       
-        password = input('Please enter your password. ')
-        while password != user_two['password']:
-            print("Incorrect password")
-            password = input('Please enter your password. ')
-            if password == user_two['password']:
-                print("")
-                print(f"Successful login your current balance is {user_two['account_balance']}.") 
-        break
-    else:
-        print("")
-        print("Invalid Username")
+    login = input('Invalid username, enter your username please.')
+    break
+if login == user_one['username']:
+    logged_in_user = user_one
+    other_user = user_two
+elif login == user_two['username']:
+    logged_in_user = user_two
+    other_user = user_one
 
-# print(user_two['connected_banks'][0][0])
+if login == logged_in_user['username']:
+        print(f"Welcome {logged_in_user['full_name']}")
+        password = input('Please enter your password. ')
+        while password != logged_in_user['password']:
+            print("Incorrect password")
+            password = input('Please enter your password. ')
+        if password == logged_in_user['password']:
+            print("")
+            print(f"Successful login your current balance is {logged_in_user['account_balance']}.")    
+            print(f"Your available connected accounts funds are:")
+            for bank, balance in logged_in_user['connected_banks']:
+                print(bank, balance)
+        else:
+            print("")
+            print("Invalid Username")
+
+
+
+
+
+
+    
+
+
