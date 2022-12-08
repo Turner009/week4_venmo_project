4270c104ac2bdb9a9b151430a460b240671b80f2
Turner009 <009turner@gmail.com>
2022-12-07 19:09:10 -0600

    rebuilt login and password process using logged_in_user variable

--- main.py
+++ main.py
@@ -18,38 +18,34 @@ password = ''
 logged_in_user =''
 other_user = ''
 
-login = input('Enter your username please.')
+
+
+
 while login != user_one['username'] or user_two['username']:
-    login = input('Invalid username, enter your username please.')
-    break
-if login == user_one['username']:
-    logged_in_user = user_one
-    other_user = user_two
-elif login == user_two['username']:
-    logged_in_user = user_two
-    other_user = user_one
+    login = input('Enter your username please.')
+    if login == user_one['username']:
+        logged_in_user = user_one
+        other_user = user_two
+        break
+    elif login == user_two['username']:
+        logged_in_user = user_two
+        other_user = user_one
+        break
+    else:
+        print("")
+        print("Invalid username")  
 
-if login == logged_in_user['username']:
-        print(f"Welcome {logged_in_user['full_name']}")
-        password = input('Please enter your password. ')
-        while password != logged_in_user['password']:
-            print("Incorrect password")
-            password = input('Please enter your password. ')
-        if password == logged_in_user['password']:
-            print("")
-            print(f"Successful login your current balance is {logged_in_user['account_balance']}.")    
-            print(f"Your available connected accounts funds are:")
-            for bank, balance in logged_in_user['connected_banks']:
-                print(bank, balance)
-        else:
-            print("")
-            print("Invalid Username")
+print (f"Welcome {logged_in_user['full_name']}")
+while password != logged_in_user['password']:
+    password = input('Please enter your password. ')
+    if password == logged_in_user['password']:
+        print("")
+        print(f"Successful login your current balance is {logged_in_user['account_balance']}.")    
+        print(f"Your available connected accounts funds are:")
+        for bank, balance in logged_in_user['connected_banks']:
+            print(bank, balance)
+        break     
+    else:
+        print("Incorrect password")
 
-
-
-
-
-
-    
-
-
+tranfer = input(f"Would you like to make a transfer to {other_user['full_name']}? (Y or N) ")
