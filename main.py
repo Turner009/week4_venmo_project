c97b55ff209349f333032b95c31aea8afe543546
Turner009 <009turner@gmail.com>
2022-12-12 18:55:17 -0600

    added option to decline bank tranfer and reenter amount

--- main.py
+++ main.py
@@ -20,9 +20,10 @@ def password_confirm(user):
     password = ''
     while password != user['password']:
         password = input('Please enter your password. ')
+
         if password == user['password']:
             print("")
-            print(f"Successful login your current balance is {user['account_balance']}.")    
+            print(f"Successful login your current balance is ${user['account_balance']}")    
             print(f"Your available connected accounts funds are:")
             balance_check(user)
             break     
@@ -31,7 +32,7 @@ def password_confirm(user):
 
 def balance_check(user):
     for bank, balance in user['connected_banks']:
-            print(f"{bank}: {balance}")
+            print(f"{bank}:${balance}")
 
 def funds_from_bank(user):
     select_bank = int(input(f"You can transfer funds from a bank account to complete transaction, which bank would you like to use? 0 for {(user['connected_banks'][0][0])} OR 1 for {(user['connected_banks'][1][0])}  "))
@@ -41,8 +42,8 @@ def funds_from_bank(user):
     list_bank = tuple(list_bank)
     user['connected_banks'][select_bank] = list_bank
 
-    print(f"Your new balance is {user['account_balance']}")
-    print(f"Your updated balance is {user['connected_banks'][select_bank][0]} {user['connected_banks'][select_bank][1]}")
+    print(f"Your new balance is ${user['account_balance']}")
+    print(f"Your updated balance is {user['connected_banks'][select_bank][0]} ${user['connected_banks'][select_bank][1]}")
 
 
 
@@ -56,24 +57,31 @@ def transfer_function(user):
         while transfer_amount > user['account_balance']:
             print("")
             print("There are not enough funds for that amount.")
-            funds_from_bank(logged_in_user)
-            transfer_amount = float(input(f"Please input new amount to transfer to {other_user['full_name']}."))
+            bank_transfer = input("Would you like to transfer funds from a bank account? (Y or N)")
+            if bank_transfer == "Y":
+                funds_from_bank(logged_in_user)
+            elif bank_transfer == "N":    
+                transfer_amount = float(input(f"Please input new amount to transfer to {other_user['full_name']}."))
         user['account_balance'] -= transfer_amount
         other_user['account_balance'] += transfer_amount
-        print(f"Transfer complete your updated balance is {user['account_balance']}")
+        print("")
+        print(f"Transfer complete your updated balance is ${user['account_balance']}")
         repeat_transfer = input(f"Would you like to make another transfer to {other_user['full_name']} (Y or N)")
         if repeat_transfer == "Y":
             transfer_amount = float(input("How much would you like to tranfer?"))
             while transfer_amount > logged_in_user['account_balance']:
                 print("")
                 print("There are not enough funds for that amount.")
-                funds_from_bank(logged_in_user)
-                transfer_amount = float(input(f"Please input new amount to transfer to {other_user['full_name']}."))
+                bank_transfer = input("Would you like to transfer funds from a bank account? (Y or N)")
+                if bank_transfer == "Y":
+                    funds_from_bank(logged_in_user)
+                elif bank_transfer == "N":    
+                    transfer_amount = float(input(f"Please input new amount to transfer to {other_user['full_name']}."))
             logged_in_user['account_balance'] -= transfer_amount
             other_user['account_balance'] += transfer_amount
-            print(f"Transfer complete your updated balance is {logged_in_user['account_balance']}") 
+            print(f"Transfer complete your updated balance is ${logged_in_user['account_balance']}") 
         elif repeat_transfer == "N":
-            print(f"Your balance is {logged_in_user['account_balance']}. Have a nice day.")
+            print(f"Your balance is ${logged_in_user['account_balance']}. Have a nice day.")
 
 
 
